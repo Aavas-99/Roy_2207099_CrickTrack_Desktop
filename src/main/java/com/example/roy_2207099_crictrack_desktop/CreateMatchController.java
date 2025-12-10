c2350604a8ee637e4df7b074a2d3527d77952f3a
Aavas Roy <roy2207099@stud.kuet.ac.bd>
2025-12-11 02:19:03 +0600

    Player info and Toss feature added

--- src/main/java/com/example/roy_2207099_crictrack_desktop/CreateMatchController.java
+++ src/main/java/com/example/roy_2207099_crictrack_desktop/CreateMatchController.java
@@ -1,25 +1,38 @@
 package com.example.roy_2207099_crictrack_desktop;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateMatchController {
 
     @FXML private TextField txtTeamA;
     @FXML private TextField txtTeamB;
     @FXML private TextField txtOvers;
+    @FXML private TextField txtStadium;
+    @FXML private DatePicker datepicker;
+
     @FXML
     private void onStartMatch() {
         String teamA = txtTeamA.getText().trim();
         String teamB = txtTeamB.getText().trim();
         String oversText = txtOvers.getText().trim();
+        String stadium = txtStadium.getText().trim();
 
-        if (teamA.isEmpty() || teamB.isEmpty() || oversText.isEmpty()) {
+        if (teamA.isEmpty() || teamB.isEmpty() || oversText.isEmpty() ||
+                stadium.isEmpty() || datepicker.getValue() == null )  {
             showAlert("Error", "All fields are required!");
             return;
         }
+
+        String date = datepicker.getValue().toString();  // format: YYYY-MM-DD
+
 
         int overs;
         try {
@@ -29,17 +42,36 @@ public class CreateMatchController {
             return;
         }
 
-        showAlert("Match Created",
-                "Match between " + teamA + " and " + teamB + " for " + overs + " overs.");
+        // Load next screen
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Toss.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            // Pass data to next controller
+            TossController controller = loader.getController();
+            controller.receiveMatchData(teamA, teamB, overs, stadium, date);
+
+            Stage stage = new Stage();
+            stage.setScene(scene);
+            stage.setTitle("Players & Toss Setup");
+            stage.show();
+
+            // Optional: close current window
+            ((Stage) txtTeamA.getScene().getWindow()).close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            showAlert("Error", "Cannot load next screen!");
+        }
     }
 
+    // Back button
     @FXML
     private void onBack() {
         Stage stage = (Stage) txtTeamA.getScene().getWindow();
         stage.close();
     }
 
-    // Helper function
     private void showAlert(String title, String msg) {
         Alert a = new Alert(Alert.AlertType.INFORMATION);
         a.setTitle(title);
---
+++ src/main/java/com/example/roy_2207099_crictrack_desktop/TossController.java
@@ -0,0 +1,117 @@
+package com.example.roy_2207099_crictrack_desktop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+
+public class TossController {
+
+    @FXML private VBox teamAList;
+    @FXML private VBox teamBList;
+    @FXML private Label lblteamA;
+    @FXML private Label lblteamB;
+
+    private String tA;
+    private String tB;
+    public void setTeams(String tA, String tB) {
+        this.tA = tA;
+        this.tB = tB;
+
+        lblteamA.setText(tA);
+        lblteamB.setText(tB);
+    }
+
+    @FXML private RadioButton rbTeamA;
+    @FXML private RadioButton rbTeamB;
+
+    @FXML private RadioButton rbBat;
+    @FXML private RadioButton rbBowl;
+
+    private ToggleGroup tossGroup = new ToggleGroup();
+    private ToggleGroup decisionGroup = new ToggleGroup();
+
+    private String teamA, teamB, stadium, date;
+    private int overs;
+
+    ArrayList<TextField> teamAPlayers = new ArrayList<>();
+    ArrayList<TextField> teamBPlayers = new ArrayList<>();
+
+    public void receiveMatchData(String tA, String tB, int ov, String st, String dt) {
+        this.teamA = tA;
+        this.teamB = tB;
+        this.overs = ov;
+        this.stadium = st;
+        this.date = dt;
+        lblteamA.setText(teamA);
+        lblteamB.setText(teamB);
+        rbTeamA.setText(teamA);
+        rbTeamB.setText(teamB);
+
+        generatePlayerInputs();
+        setupToggles();
+    }
+
+    private void generatePlayerInputs() {
+        for (int i = 1; i <= 11; i++) {
+            TextField a = new TextField();
+            a.setPromptText("Player " + i);
+            teamAList.getChildren().add(a);
+            teamAPlayers.add(a);
+
+            TextField b = new TextField();
+            b.setPromptText("Player " + i);
+            teamBList.getChildren().add(b);
+            teamBPlayers.add(b);
+        }
+    }
+
+    private void setupToggles() {
+        rbTeamA.setToggleGroup(tossGroup);
+        rbTeamB.setToggleGroup(tossGroup);
+
+        rbBat.setToggleGroup(decisionGroup);
+        rbBowl.setToggleGroup(decisionGroup);
+    }
+
+
+
+    @FXML
+    private void onContinue() {
+        for (TextField f : teamAPlayers) {
+            if (f.getText().trim().isEmpty()) {
+                show("Team A must have 11 players!");
+                return;
+            }
+        }
+        for (TextField f : teamBPlayers) {
+            if (f.getText().trim().isEmpty()) {
+                show("Team B must have 11 players!");
+                return;
+            }
+        }
+
+        if (tossGroup.getSelectedToggle() == null) {
+            show("Select toss winner!");
+            return;
+        }
+
+        if (decisionGroup.getSelectedToggle() == null) {
+            show("Select bat or bowl!");
+            return;
+        }
+
+        String tossWinner = rbTeamA.isSelected() ? teamA : teamB;
+        String decision = rbBat.isSelected() ? "Bat" : "Bowl";
+
+        show("Success!\nToss: " + tossWinner + " chose to " + decision);
+    }
+
+    private void show(String msg) {
+        Alert a = new Alert(Alert.AlertType.INFORMATION);
+        a.setHeaderText(null);
+        a.setContentText(msg);
+        a.show();
+    }
+}

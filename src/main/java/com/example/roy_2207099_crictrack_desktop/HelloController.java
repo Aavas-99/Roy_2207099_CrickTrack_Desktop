c26be48a156573fb0abd945604b1a0a520fba3de
Aavas Roy <roy2207099@stud.kuet.ac.bd>
2025-12-10 23:07:54 +0600

    Welcome Scene Created

---
+++ src/main/java/com/example/roy_2207099_crictrack_desktop/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.roy_2207099_crictrack_desktop;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    @FXML
+    protected void onCreateMatchClick() {
+
+    }
+
+    @FXML
+    protected void onHistoryClick() {
+
+    }
+
+    @FXML
+    protected void onExitClick() {
+        System.exit(0);
+    }
+}
---
+++ src/main/java/com/example/roy_2207099_crictrack_desktop/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.roy_2207099_crictrack_desktop;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
